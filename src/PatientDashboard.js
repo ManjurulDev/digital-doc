91cce90376b93fa41fdcf8f4a2dc7f0bde968bcd
Manjurul Islam <mijony29@gmail.com>
2023-12-14 03:28:39 +0600

    auth implemented

--- src/PatientDashboard.js
+++ src/PatientDashboard.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {FlatList, StyleSheet, Text, View} from 'react-native';
+import {useNavigation} from "@react-navigation/native";
 
 const doctorsData = [
   // Example data structure
@@ -8,17 +9,22 @@ const doctorsData = [
   // Add more doctors as needed
 ];
 
-const DoctorItem = ({name, specialty, available}) => (
-  <View style={styles.doctorItem}>
-    <View style={styles.doctorInfo}>
-      <Text style={styles.doctorName}>{name}</Text>
-      <Text style={styles.doctorSpecialty}>{specialty}</Text>
-    </View>
-    <Text style={styles.doctorAvailability}>
-      {available ? 'Available' : 'Unavailable'}
-    </Text>
-  </View>
-);
+const DoctorItem = ({name, specialty, available}) => {
+  const navigation = useNavigation();
+  return(
+      <View style={styles.doctorItem}>
+        <View style={styles.doctorInfo}>
+          <Text style={styles.doctorName}>{name}</Text>
+          <Text style={styles.doctorSpecialty}>{specialty}</Text>
+        </View>
+
+        <View>
+          {available ? <Text style={styles.doctorAvailability} onPress={() => navigation.navigate('CallPage')}>Available</Text> :
+              <Text style={styles.doctorAvailability}>Unavailable</Text>}
+        </View>
+      </View>
+  )
+};
 
 const PatientDashboard = () => {
   return (
