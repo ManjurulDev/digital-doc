972dcbbac9080801865ae50359e35dacb249db48
S M Ariq Islam <arikislam321@gmail.com>
2023-12-21 23:17:56 +0600

    Change data

--- src/components/DiagnosticsAndTests.js
+++ src/components/DiagnosticsAndTests.js
@@ -8,19 +8,38 @@ import BlockHeader from './BlockHeader';
 import DiagnosticItem from './DiagnosticItem';
 import {getMeetings} from "../screens/app/api/doctor.api";
 import {useSelector} from "react-redux";
+import httpClient from "../screens/app/api/client";
+import { getOptions } from "../screens/app/api/patients.api";
 
 const DiagnosticsAndTests = ({version}) => {
+  const [meetings, setMeetings] = useState([]);
+  const authToken = useSelector((state) => state.auth.authToken);
 
-    const [meetings, setMeetings] = useState([]);
 
-    // useEffect(() => {
-    //     getMeetings().then((data) => {
-    //         console.log(data)
-    //         //setMeetings(data)
-    //     })
-    // }, []);
+  useEffect(() => {
+
+    // Since we have the authToken now, we can use it directly in our fetch call
+    fetch('https://server.zyngchat.com/api/doctors/meetings', {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + authToken,
+      },
+    })
+      .then((response) => response.json())
+      .then((json) => {
+        console.log(json);
+        // setMeetings(json) // Uncomment and use this to set meetings if your API returns the meetings data
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+
+  }, [authToken]);
 
   const v1 = () => {
+
     return (
       <View style={{marginTop: 20, marginHorizontal: 20}}>
         <BlockHeader
