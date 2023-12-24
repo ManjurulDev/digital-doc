34a52befc555c2176aa56e22f393504be140953c
S M Ariq Islam <arikislam321@gmail.com>
2023-12-24 18:38:53 +0600

    Create prescription add download done

--- src/components/PatientList.js
+++ src/components/PatientList.js
@@ -1,4 +1,4 @@
-import {View, Text, TouchableOpacity, Button, StyleSheet, TextInput} from 'react-native';
+import { View, Text, TouchableOpacity, Button, StyleSheet, TextInput, ScrollView } from "react-native";
 import React, {useState} from 'react';
 import {useNavigation} from '@react-navigation/native';
 
@@ -7,7 +7,8 @@ import {svg} from '../assets';
 import BellSvg from "../assets/svg/BellSvg";
 import TestIsReadySvg from "../assets/svg/TestIsReadySvg";
 import Modal from "react-native-modal";
-
+import { submitPrescription } from "../screens/app/api/doctor.api";
+import { Linking } from 'react-native';
 
 const PatientList = ({
   icon,
@@ -23,37 +24,45 @@ const PatientList = ({
 
     const [isModalVisible, setIsModalVisible] = useState(false);
     const handleModal = () => setIsModalVisible(true);
+    const [medicines, setMedicines] = useState([]);
+    const [newMedicine, setNewMedicine] = useState({
+      title: '',
+      dose: '',
+      dose_instruction: '',
+      duration: ''
+    });
 
-    const initialData = {
-        title: '',
-        dose: '',
-        instruction: '',
-        duration: '',
-    };
+    const customInputStyle = {
+      input: {
+        borderWidth: 1,
+        borderColor: '#ddd',
+        padding: 10,
+        marginBottom: 10,
+        borderRadius: 5,
+      },
+    }
 
-    const [inputData, setInputData] = useState(initialData);
 
-    const handleInputChange = (name, value) => {
-        setInputData((prevData) => ({
-            ...prevData,
-            [name]: value,
-        }));
-    };
+  const handleAddMedicine = () => {
+    setMedicines([...medicines, newMedicine]);
+    setNewMedicine({ title: '', dose: '', dose_instruction: '', duration: '' }); // Reset form
+  };
+
+  const handleRemoveMedicine = (index) => {
+    const updatedMedicines = medicines.filter((_, i) => i !== index);
+    setMedicines(updatedMedicines);
+  };
+
+
 
     const handleSubmit = () => {
-        let medicineInput = {
-            title: inputData.title,
-            dose: inputData.dose,
-            dose_instruction: inputData.instruction,
-            duration: inputData.duration,
-        }
+      submitPrescription(meeting.id, { medicines }).then((res)=> {
+        console.log(res);
+      })
+      setIsModalVisible(false);
 
-        let formData = {
-            medicines:[medicineInput]
-        }
+      Linking.openURL(`https://server.zyngchat.com/api/meetings/${meeting.id}/download-prescription`).catch(err => console.error("Couldn't load page", err));
 
-        console.log('formData:', formData);
-        setIsModalVisible(false)
     }
 
   const v1 = () => {
@@ -117,21 +126,23 @@ const PatientList = ({
           <View>
               <Text onPress={handleModal}> <svg.DashboardSvg/> </Text>
               <Modal isVisible={isModalVisible} style={styles.container}>
-                  <View style={{ flex: 1 }}>
+                <View style={{ flex: 1, backgroundColor: 'white', borderRadius: 10, padding: 20 }}>
+                  <ScrollView>
+                    {medicines.map((medicine, index) => (
+                      <View key={index} style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', marginBottom: 10 }}>
+                        <Text>{medicine.title} - {medicine.dose}</Text>
+                        <Button title="Remove" onPress={() => handleRemoveMedicine(index)} />
+                      </View>
+                    ))}
+                  </ScrollView>
 
-                      {Object.keys(inputData).map((name, index) => (
-                          <View key={index}>
-                              <Text style={{textTransform:'uppercase'}}>{`${name}:`}</Text>
-                              <TextInput
-                                  style={styles.input}
-                                  multiline={true}
-                                  numberOfLines={2}
-                                  value={inputData[name]}
-                                  onChangeText={(text) => handleInputChange(name, text)}
-                              />
-                          </View>
-                      ))}
-                      <Button title="Submit" onPress={handleSubmit} />
+                  <TextInput style={customInputStyle.input} placeholder="Title" value={newMedicine.title} onChangeText={(text) => setNewMedicine({ ...newMedicine, title: text })} />
+                  <TextInput style={customInputStyle.input} placeholder="Dose" value={newMedicine.dose} onChangeText={(text) => setNewMedicine({ ...newMedicine, dose: text })} />
+                  <TextInput style={customInputStyle.input} placeholder="Instruction" value={newMedicine.dose_instruction} onChangeText={(text) => setNewMedicine({ ...newMedicine, dose_instruction: text })} />
+                  <TextInput  style={customInputStyle.input} placeholder="Duration" keyboardType="numeric" value={newMedicine.duration.toString()} onChangeText={(text) => setNewMedicine({ ...newMedicine, duration: text })} />
+
+                  <Button title="Add Medicine" onPress={handleAddMedicine} />
+                  <Button title="Submit" onPress={handleSubmit} />
                   </View>
               </Modal>
           </View>
--- src/screens/app/api/doctor.api.js
+++ src/screens/app/api/doctor.api.js
@@ -4,10 +4,13 @@ import httpClient from "./client";
 const ROUTE_PREFIX = 'doctors'
 const LOGIN_END_POINT = 'login'
 const MEETING_END_POINT = 'meetings'
+const PRESCRIPTION_END_POINT = 'prescription'
 const postDoctorLogin = (payload) => httpClient.post(ROUTE_PREFIX + '/' + LOGIN_END_POINT, payload);
 const getMeetings = () => httpClient.get(ROUTE_PREFIX + '/' + MEETING_END_POINT);
+const submitPrescription = (meetingId, payload) => httpClient.post(ROUTE_PREFIX + '/' + MEETING_END_POINT+ '/'+ meetingId+ '/' + PRESCRIPTION_END_POINT, payload);
 
 export {
     postDoctorLogin,
-    getMeetings
+    getMeetings,
+    submitPrescription
 }
